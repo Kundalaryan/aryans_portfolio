b999abba3cb138f05aa86e5fffca2068bd296588
aryankundal <aryan4321.ak@gmail.com>
2025-04-13 23:38:54 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,53 @@
+window.PorfolioConfig = {
+  name: 'AryanKundal',
+  jobTitle: 'Student',
+  jobDescription: 'I’m a passionate Computer Science student with a strong interest in AI, machine learning, and cybersecurity. Currently pursuing my B.E. from MBSCET, I enjoy building practical projects and aim to create impactful solutions while constantly learning and improving.',
+  skills: [
+    {
+      name: 'Web Devlopment',
+      description: 'Intermediate level, HTML, CSS, Js and other frameworks.'
+    },
+    {
+      name: 'App Devlopment',
+      description: 'Backend Devlopment using Flutter+Dart.'
+    },
+    {
+      name: 'Pentesting',
+      description: 'Web Application Security: OWASP Top 10, XSS, SQL Injection, CSRF'
+    },
+    {
+      name: 'AI/ML',
+      description: 'Intrusion Detection System using Machine learning using Python, Library likes Scikit-learn etc.'
+    }
+  ],
+  works: [
+    {
+      name: 'SudharHub',
+      description: 'Developed SudharHub, a community-driven mobile application, gaining hands-on experience in UI/UX design, backend development, database integration, and deployment.'
+    },
+    {
+      name: 'Competency Development Programme',
+      description: 'As a Project Management Intern at IIT Jammu, I contributed to the effective coordination and administration of the 6-month Competency Development Programme on Network and Mobile Security for DoT officers.'
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Photography',
+      description: 'In my Free time I use to do astrography with my Phone.'
+    }
+  ],
+  contacts: {
+    email: 'aryan4321.ak@gmail.com',
+    mobileNo: '+91 9149607506',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/kundalaryan'
+      },
+      {
+        name: 'Linkedin',
+        link: 'https://linkedin.com/in/aryan-kundal-5a8524186'
+      }
+    ]
+  }
+};
